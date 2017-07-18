c619f3c5102fd1df32f9338058cbb611ca598024
mwnnugraha <151610223@smkasaalaambandung.sch.id>
2017-07-18 11:52:57 +0700

    baru

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="conteiner">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+             <di <div class="panel panel-default">
+                <div class="panel-heading">Halo</div>
+                <div class="panel-body">
+                    Selamat datang di Piket Sekolah SMK ASSALAAM Bandung
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection 
